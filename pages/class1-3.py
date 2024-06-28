58c11f9808a88642c11c00dfd1c8ad7ea78e526f
andrewputan <andrewma12345678@gmail.com>
2024-06-28 19:03:12 +0800

    hihi

--- pages/class1-3.py
+++ pages/class1-3.py
@@ -1,17 +1,19 @@
-print(1 == 1)  # 比較有沒有相等
-print(1 != 0)  # 比較有沒有不相等
-print(1 < 2)  # 比較是否小於
-print(1 > 2)  # 比較是否大於
-print(1 <= 2)  # 比較是否小於或等於
-print(1 >= 2)  # 比較是否大於或等於
+import streamlit as st
+
+st.markdown(1 == 1)  # 比較有沒有相等
+st.markdown(1 != 0)  # 比較有沒有不相等
+st.markdown(1 < 2)  # 比較是否小於
+st.markdown(1 > 2)  # 比較是否大於
+st.markdown(1 <= 2)  # 比較是否小於或等於
+st.markdown(1 >= 2)  # 比較是否大於或等於
 
 
-password = input("請輸入密碼:")
+password = st.text_input("請輸入密碼:")
 if password == "hihi":
-    print("密碼正確")
+    st.markdown("密碼正確")
 elif password == "hihihi":
-    print("密碼正確")
+    st.markdown("密碼正確")
 elif password == "hi":
-    print("密碼正確")
+    st.markdown("密碼正確")
 else:
-    print("密碼錯誤")
+    st.markdown("密碼錯誤")
